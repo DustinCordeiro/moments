e893e59d3787de28fd214dcbdf5dd91f2255ca1e
Dustin Leandro <dustin.cordeiro88@gmail.com>
2022-11-15 21:08:31 -0300

    reactive forms + validation

---
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.scss']
+})
+export class NewMomentComponent implements OnInit {
+  btnText: string = 'Compartilhar';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
